70b1c88b212f898daebad1b56da89f3805446d83
CptDads <tjj.jonker@gmail.com>
2021-10-09 15:36:50 +0200

    Finished simple FPS Movement
    Player is now able to walk around the scene with colission and gravity

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/FPS Input")]
 public class FPSInput : MonoBehaviour
 {
     public float speed = 6f;
     private CharacterController characterController;
+
+    public float gravity = -9.8f;
+
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
@@ -17,6 +22,8 @@ public class FPSInput : MonoBehaviour
         float deltaZ = Input.GetAxis("Vertical") * speed;
         Vector3 movement = new Vector3(deltaX, 0, deltaZ);
         movement = Vector3.ClampMagnitude(movement, speed);
+
+        movement.y = gravity;
 
         movement *= Time.deltaTime;
         movement = transform.TransformDirection(movement);
