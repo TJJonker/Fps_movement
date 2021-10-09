e0d8c2e06005a796536d4310d4133c093b322037
CptDads <tjj.jonker@gmail.com>
2021-10-09 15:29:14 +0200

    Update FPSInput.cs
    Player can now fly around with collision

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class FPSInput : MonoBehaviour
 {
     public float speed = 6f;
+    private CharacterController characterController;
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
 
     private void Update()
     {
-        float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        float deltaZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        transform.Translate(deltaX, 0, deltaZ);
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        characterController.Move(movement);
     }
 }
